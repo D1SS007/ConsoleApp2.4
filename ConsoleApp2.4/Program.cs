2a18e84aa1e7f5c4ea01ef210c9ae4bb05e87ad9
Alex Zverev <lordzver007@yandex.ru>
2022-03-28 16:42:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sumOfNumbers = 0;
+
+            Random rand = new Random();
+            number = rand.Next(0, 100);
+
+            Console.WriteLine(number);
+
+            for (int i = 0; i <= number; i++)
+            {
+               if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sumOfNumbers += i;                    
+                }               
+            }
+            Console.WriteLine(sumOfNumbers);
+        }
+    }
+}
